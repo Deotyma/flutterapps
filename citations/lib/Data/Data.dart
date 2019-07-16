0bb6fc5f969e8e9a21412e55a8fd87de283a4186
Ewa Kadziolka <kalistaewa@gmail.com>
2019-07-16 19:27:50 +0200

    first commit

---
+++ citations/lib/Data/Data.dart
@@ -0,0 +1,25 @@
+List<String> images = [
+  "assets/kwiaty1.jpg",
+  "assets/kwiaty2.jpg",
+  "assets/kwiaty3.jpg",
+  "assets/kwiaty4.jpg",
+  "assets/kwiaty5.jpg",
+  "assets/kwiaty6.jpg",
+  "assets/kwiaty7.jpg",
+  "assets/kwiaty8.jpg",
+  "assets/kwiaty9.jpg",
+  "assets/kwiaty10.jpg",
+];
+
+List<String> title = [
+  "La charité patfaiteconsiste à supporter les défauts des autres.",
+  "Nous n'avon que cette vie pour vivre de la foi.",
+  "La joie interiéure résiede au plus intime de l'âme.  ",
+  "Notre mort, c'est comme une goutte d'eau pompée par le soleil de Dieu.",
+  "Il faut toujours prier comme si l'action était inutile et agir comme si la ptière était insuffisante.",
+  "La sainteté est une disposition du coeur qui nous rends humble et petit entre le bras du Père.",
+  "Nos élans vers la souffrance, il faut y être pour savoir ce qu'ils valent. ",
+  "C'est à toi seul, Jèsus, que je m'attache.",
+  "Je reviendrai sur la terre pour faire aimer l’amour. ",
+  "Le Christ est mon amour, il est toute ma vie."
+];
